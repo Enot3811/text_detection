c25a1921d6d33334f746f5b1b3fdd983285773e2
Enot3811 <enot3811@gmail.com>
2023-08-04 17:30:49 +0300

    Start new dataset collecting

--- utils/numpy_utils.py
+++ utils/numpy_utils.py
@@ -1,5 +1,9 @@
 """Module with utilities related to numpy."""
 
+
+from typing import List, Tuple
+
+import numpy as np
 from numpy.typing import ArrayLike
 from torch import Tensor
 
@@ -23,6 +27,39 @@ def tensor_to_numpy(
     ArrayLike
         Converted tensor.
     """
+    data = data.detach().cpu()
     if permute_channel_dim:
         data = data.permute(1, 2, 0)
     return data.numpy()
+
+
+def rotate_rectangle(
+    points: List[Tuple[int, int]], angle: float, radians: bool = True
+) -> List[Tuple[int, int]]:
+    """Rotate points of rectangle by given angle.
+
+    Parameters
+    ----------
+    points : List[Tuple[int, int]]
+        Points of rectangle. There must be 2 or 4.
+    angle : float
+        Angle to rotate.
+    radians : bool, optional
+        Whether angle given in radians. Otherwise in degrees. By default equals
+        `True` (radians).
+
+    Returns
+    -------
+    List[Tuple[int, int]]
+        List of rotated rectangle points.
+    """
+    if not radians:
+        angle = np.deg2rad(angle)
+    c_x, c_y = np.mean(points, axis=0)
+
+    points = [
+        (
+            int(c_x + np.cos(angle) * (px - c_x) - np.sin(angle) * (py - c_y)),
+            int(c_y + np.sin(angle) * (px - c_x) + np.cos(angle) * (py - c_y)))
+        for px, py in points]
+    return points
