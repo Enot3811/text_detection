469454be81fb32019b0c223c4d401ba3570c1d12
Enot3811 <enot3811@gmail.com>
2023-08-10 17:32:07 +0300

    Implement base dataset classes

--- utils/data_utils/datasets/MSRA_TD500.py
+++ utils/data_utils/datasets/MSRA_TD500.py
@@ -2,15 +2,13 @@ from pathlib import Path
 import sys
 from typing import Tuple, List, Union
 
-from numpy.typing import ArrayLike
-
 sys.path.append(str(Path(__file__).parents[3]))
-from utils.image_utils import read_image
 from utils.numpy_utils import rotate_rectangle
-from rcnn.rcnn_utils import draw_bounding_boxes_cv2
+from utils.data_utils.datasets.base_dataset import (
+    BaseAnnotation, BaseExample, BaseDataset)
 
 
-class MSRA_TD500_annotation:
+class MSRA_TD500_annotation(BaseAnnotation):
 
     def __init__(
         self, annt_str: str
@@ -23,8 +21,8 @@ class MSRA_TD500_annotation:
         y = int(y)
         w = int(w)
         h = int(h)
-        self.x1, self.y1, self.x2, self.y2 = self.normalize_bboxes(
-            x, y, w, h, angle)
+        x1, y1, x2, y2 = self.normalize_bboxes(x, y, w, h, angle)
+        super().__init__(x1, y1, x2, y2, 'unlabeled')
     
     def normalize_bboxes(
         self, x: int, y: int, w: int, h: int, angle: float
@@ -41,6 +39,8 @@ class MSRA_TD500_annotation:
             Width of bbox.
         h : int
             Height of bbox.
+        angle : float
+            Angle to rotate bbox.
 
         Returns
         -------
@@ -66,24 +66,17 @@ class MSRA_TD500_annotation:
         return x1, y1, x2, y2
 
 
-class MSRA_TD500_example:
+class MSRA_TD500_example(BaseExample):
     def __init__(
         self, img_pth: Path, img_annots: List[MSRA_TD500_annotation]
     ) -> None:
-        self.img_pth = img_pth
-        self.img_annots = img_annots
-
-    def get_image(self) -> ArrayLike:
-        return read_image(self.img_pth)
-    
-    def get_image_with_bboxes(self) -> ArrayLike:
-        img = read_image(self.img_pth)
-        bboxes = list(map(lambda anns: (anns.x1, anns.y1, anns.x2, anns.y2)))
-        return draw_bounding_boxes_cv2(img, bboxes)
+        super().__init__(img_pth, img_annots)
 
 
-class MSRA_TD500_dataset:
+class MSRA_TD500_dataset(BaseDataset):
     def __init__(self, dset_folder: Union[Path, str]) -> None:
+        super().__init__()
+
         if isinstance(dset_folder, str):
             dset_folder = Path(dset_folder)
 
@@ -92,6 +85,7 @@ class MSRA_TD500_dataset:
 
         self.train_set = self.read_set(train_dir)
         self.test_set = self.read_set(test_dir)
+        self.val_set = []
 
     def read_set(self, set_dir: Path) -> List[MSRA_TD500_example]:
         """Read a directory with a set, generate a list of examples.
---
+++ utils/data_utils/datasets/base_dataset.py
@@ -0,0 +1,48 @@
+"""A module that contains a base dataset, example and annotation classes."""
+
+
+from pathlib import Path
+import sys
+from typing import List
+
+from numpy.typing import ArrayLike
+
+sys.path.append(str(Path(__file__).parents[3]))
+from utils.image_utils import read_image
+from rcnn.rcnn_utils import draw_bounding_boxes_cv2
+
+
+class BaseAnnotation:
+    def __init__(
+        self, x1: int, y1: int, x2: int, y2: int, language: str
+    ) -> None:
+        self.x1 = x1
+        self.y1 = y1
+        self.x2 = x2
+        self.y2 = y2
+        self.language = language
+
+
+class BaseExample:
+    def __init__(
+        self, img_pth: Path, img_annots: List[BaseAnnotation]
+    ) -> None:
+        self.img_pth = img_pth
+        self.img_annots = img_annots
+
+    def get_image(self) -> ArrayLike:
+        return read_image(self.img_pth)
+    
+    def get_image_with_bboxes(self) -> ArrayLike:
+        img = self.get_image()
+        bboxes = list(map(lambda anns: (anns.x1, anns.y1, anns.x2, anns.y2),
+                          self.img_annots))
+        labels = list(map(lambda anns: anns.language, self.img_annots))
+        return draw_bounding_boxes_cv2(img, bboxes, labels)
+
+
+class BaseDataset:
+    def __init__(self) -> None:
+        self.train_set: List[BaseExample]
+        self.val_set: List[BaseExample]
+        self.test_set: List[BaseExample]
