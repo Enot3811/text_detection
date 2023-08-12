370b7390d47590981a7bdaa4cd8769a845948631
Enot3811 <enot3811@gmail.com>
2023-08-12 17:42:00 +0300

    Add datasets module's init

---
+++ utils/data_utils/datasets/__init__.py
@@ -0,0 +1,4 @@
+from utils.data_utils.datasets.ICDAR2003 import ICDAR2003_dataset  # noqa
+from utils.data_utils.datasets.MSRA_TD500 import MSRA_TD500_dataset  # noqa
+from utils.data_utils.datasets.StreetViewText import SVT_dataset  # noqa
+from utils.data_utils.datasets.NEOCR import NEOCR_dataset  # noqa
