7a8a7fbe420061a1b6f37fdd323ee2f4bd6eab30
Enot3811 <enot3811@gmail.com>
2023-08-14 20:00:56 +0300

    Add datasets dict to module, add new imports

--- utils/data_utils/datasets/__init__.py
+++ utils/data_utils/datasets/__init__.py
@@ -1,4 +1,15 @@
-from utils.data_utils.datasets.ICDAR2003 import ICDAR2003_dataset  # noqa
-from utils.data_utils.datasets.MSRA_TD500 import MSRA_TD500_dataset  # noqa
-from utils.data_utils.datasets.StreetViewText import SVT_dataset  # noqa
-from utils.data_utils.datasets.NEOCR import NEOCR_dataset  # noqa
+from typing import Dict
+
+from utils.data_utils.datasets.base_dataset import (  # noqa
+    BaseDataset, BaseSample, BaseAnnotation)
+from utils.data_utils.datasets.ICDAR2003 import ICDAR2003_dataset
+from utils.data_utils.datasets.MSRA_TD500 import MSRA_TD500_dataset
+from utils.data_utils.datasets.StreetViewText import SVT_dataset
+from utils.data_utils.datasets.NEOCR import NEOCR_dataset
+
+datasets: Dict[str, BaseDataset] = {
+    'ICDAR2003': ICDAR2003_dataset,
+    'MSRA_TD500': MSRA_TD500_dataset,
+    'NEOCR': NEOCR_dataset,
+    'StreetViewText': SVT_dataset
+}
