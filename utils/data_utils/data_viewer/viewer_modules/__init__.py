95e2f9611102d7de9028ee243bfcf5f6e6c61351
Enot3811 <enot3811@gmail.com>
2023-09-22 14:16:49 +0300

    Delete dataset viewer

--- utils/data_utils/data_viewer/viewer_modules/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Viewer's component modules."""
-
-from utils.data_utils.data_viewer.viewer_modules.viewer_dataset import ( # noqa
-    ViewerDataset)
-from utils.data_utils.data_viewer.viewer_modules.viewer_window import ( # noqa
-    ViewerWindow)
